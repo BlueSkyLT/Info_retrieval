9443c21367d12ce170e7e68d24935aea89a0011b
BlueSkyLT <llantian2014@gmail.com>
2020-03-25 18:06:51 +0800

    use novels

--- indexing.py
+++ indexing.py
@@ -6,17 +6,15 @@ from whoosh.analysis import LanguageAnalyzer
 def createSearchableData(DIR):   
     '''
     Schema: 
-        title(filenames), 
+        title: title of the novel, 
         path(as ID), 
+        author: author of the novel,
         content(tokens, stemmed, indexed but not stored),
-        textdata (stored full text content)
-        Date?(for emails)
-        Subject?
-        Author?(for emails/novels)
+        textdata (stored full novel content)
     '''
     # use English Snowball stemmer
     stem_ana = LanguageAnalyzer("en")
-    schema = Schema(title=TEXT(stored = True), path = ID(stored = True), content = TEXT(analyzer = stem_ana, sortable = True), textdata = TEXT(stored = True), date = DATETIME(sortable = True))
+    schema = Schema(title = TEXT(stored = True), path = ID(stored = True), author = ID(stored = True), content = TEXT(analyzer = stem_ana, sortable = True), textdata = TEXT(stored = True))
     
     # directory for the index
     if not os.path.exists("indexdir"):
@@ -32,6 +30,11 @@ def createSearchableData(DIR):
         fp = open(path,'r', encoding = "utf8", errors = 'ignore')
         print(path)
         text = fp.read()
+
+        # get title, author and content from text
+        # title, author, content = get_content(text)
+        # writer.add_document(title = title, author = author, path = path, content = text, textdata = text)
+
         writer.add_document(title = path.split('/')[1], path = path, content = text, textdata = text) 
         fp.close()
     writer.commit()
--- search.py
+++ search.py
@@ -7,9 +7,20 @@ ix = open_dir("indexdir")
  
 # query_str is query string
 query_str = sys.argv[1]
-# Top 'n' documents as result
-topN = int(sys.argv[2])
- 
+
+# 0: search by title
+# 1: search by author
+# 2: search by content
+search_by = sys.argv[2]
+
+# Top 'n' documents as result, default 10
+topN = int(sys.argv[3])
+
+# search by title
+
+# search by author
+
+# search by content 
 with ix.searcher(weighting = scoring.Frequency) as searcher:
 	query = QueryParser("content", ix.schema).parse(query_str)
 	results = searcher.search(query,limit = topN)
