a39f8e5f92e8b39f7f2be008d01fea847f3bad57
zeyang <981435961@qq.com>
2020-03-27 00:02:24 +0800

    first version (HillaryEmail)

--- Main.py
+++ Main.py
@@ -1,6 +1,5 @@
 import argparse
 from components import *
-from itertools import chain
 
 
 parser = argparse.ArgumentParser(
@@ -10,18 +9,19 @@ parser.add_argument('-d', '--dataset', default='HillaryEmails/',
 
 
 def main(args):
-    files = get_files(args.dataset)
-    file_token_list = list(list())  # containing token list for each file
-    for file in files[:10]:  # TODO for test
-        with open(file) as f:
-            file_token_list.append(linguistic(tokenization(f.read(), file)))
-    all_token_list = list(chain.from_iterable(file_token_list))  # containing tokens from all documents
-    sorting(all_token_list)  # in-place sorting, save memory
-    posting = Posting()
-    for token in all_token_list:
-        posting.add(token)
-    merge([[1, 2, 5, 7], [2, 4, 5], [2, 5]])
-    return
+    dataset = Dataset(args.dataset)
+    while True:
+        query = input('Please input query for search (using space to separate multi queries):\n')
+        if not query:
+            continue
+        start_time = time.time()
+        output = dataset.query(query)
+        time_used = time.time() - start_time
+        if output:
+            print('Num of Documents: {}\t Time: {:.4f}s'.format(len(output), time_used))
+            print(output)
+        else:
+            print('Your search "{}" did not match any documents.'.format(query))
 
 
 if __name__ == "__main__":
--- components.py
+++ components.py
@@ -1,94 +1,221 @@
 import os
 import string
+import time
+import pickle
 from nltk.stem.snowball import SnowballStemmer
+from itertools import chain
 
 
-def get_files(dirname: str):
+# def get_files(dirname: str):
+#     """
+#     Args:
+#         dirname (str): path to directory
+#     Returns:
+#         file_list (list): full paths to files in the directory
+#     """
+#     file_list = list()
+#     for filename in os.listdir(dirname):
+#         file_list.append(os.path.join(dirname, filename))
+#     return file_list
+#
+#
+# def tokenization(content: str, doc_id: str):
+#     """
+#     Args:
+#         content (str): Document text
+#         doc_id(str): Document Id
+#     Returns:
+#         tokens (list): Token-docId pairs
+#     """
+#     tokens = list()
+#     lines = content.splitlines()
+#     for line in lines:
+#         token = line.split()  # default split by whitespace
+#         tokens.extend(zip(token, len(token) * [doc_id]))
+#     return tokens
+#
+#
+# def linguistic(tokens: list):
+#     """
+#     Remove punctuation symbols, lowercasing and stemming.
+#     Args:
+#         tokens (list): Original token-docId pairs
+#     Returns:
+#         tokens_modified (list): Modified token-docId pairs
+#     """
+#     tokens_modified = list()
+#     stemmer = SnowballStemmer("english")
+#     translator = str.maketrans('', '', string.punctuation)
+#     for pairs in tokens:
+#         token = pairs[0].translate(translator).lower()  # remove punctuation symbols, lowercasing
+#         if token:  # filter out those empty token
+#             tokens_modified.append((stemmer.stem(token), pairs[1]))  # stemming
+#     return tokens_modified
+#
+#
+# def sorting(tokens: list):
+#     """
+#     Perform sorting of the token list, first by tokens (alphabetical order),
+#     and then by document ids (alphabetical order).
+#     Args:
+#         tokens (list): token list to be sorted
+#     Returns:
+#         tokens_sorted (list): sorted token list
+#     """
+#     tokens.sort(key=lambda x: (x[0], x[1]))
+#
+#
+# class Posting(object):
+#     """
+#     Create inverted index from sorted list of of token-docId pairs.
+#     """
+#     def __init__(self):
+#         self.posting = dict()
+#
+#     def add(self, token):
+#         if token[0] in self.posting:
+#             if token[1] not in self.posting[token[0]][1]:  # collapse identical tokens together
+#                 self.posting[token[0]][1].append(token[1])
+#                 self.posting[token[0]][0] += 1
+#         else:
+#             self.posting[token[0]] = [1, [token[1]]]
+
+
+class Dataset(object):
     """
+    Create index for a dataset.
     Args:
-        dirname (str): path to directory
-    Returns:
-        file_list (list): full paths to files in the directory
+        dirname (str): path to the dataset directory
     """
-    file_list = list()
-    for filename in os.listdir(dirname):
-        file_list.append(os.path.join(dirname, filename))
-    return file_list
-
-
-def tokenization(content: str, doc_id: str):
-    """
-    Args:
-        content (str): Document text
-        doc_id(str): Document Id
-    Returns:
-        tokens (list): Token-docId pairs
-    """
-    tokens = list()
-    lines = content.splitlines()
-    for line in lines:
-        token = line.split()  # default split by whitespace
-        tokens.extend(zip(token, len(token) * [doc_id]))
-    return tokens
-
-
-def linguistic(tokens: list):
-    """
-    Remove punctuation symbols, lowercasing and stemming.
-    Args:
-        tokens (list): Original token-docId pairs
-    Returns:
-        tokens_modified (list): Modified token-docId pairs
-    """
-    tokens_modified = list()
-    stemmer = SnowballStemmer("english")
-    translator = str.maketrans('', '', string.punctuation)
-    for pairs in tokens:
-        token = pairs[0].translate(translator).lower()  # remove punctuation symbols, lowercasing
-        if token:  # filter out those empty token
-            tokens_modified.append((stemmer.stem(token), pairs[1]))  # stemming
-    return tokens_modified
-
-
-def sorting(tokens: list):
-    """
-    Perform sorting of the token list, first by tokens (alphabetical order),
-    and then by document ids (alphabetical order).
-    Args:
-        tokens (list): token list to be sorted
-    Returns:
-        tokens_sorted (list): sorted token list
-    """
-    tokens.sort(key=lambda x: (x[0], x[1]))
-
-
-class Posting(object):
-    """
-    Create inverted index from sorted list of of token-docId pairs.
-    """
-    def __init__(self):
+    def __init__(self, dirname):
+        self.dirname = dirname
+        self.stemmer = SnowballStemmer("english")
+        self.translator = str.maketrans('', '', string.punctuation)
         self.posting = dict()
+        self.create_index()
 
-    def add(self, token):
-        if token[0] in self.posting:
-            if token[1] not in self.posting[token[0]][1]:  # collapse identical tokens together
-                self.posting[token[0]][1].append(token[1])
-                self.posting[token[0]][0] += 1
+    def create_index(self):
+        cache_file = self.dirname + 'index_cache.pkl'
+        if os.path.exists(cache_file):
+            with open(cache_file, 'rb') as f:
+                self.posting = pickle.load(f)
+            print('Index of {} corpus loaded from {}'.format(os.path.dirname(self.dirname), cache_file))
+            return
+        print('Creating index for {} corpus...'.format(os.path.dirname(self.dirname)))
+        start_time = time.time()
+        files = self.get_files(self.dirname)
+        file_token_list = list(list())  # containing token list for each file
+        for file in files:
+            with open(file) as f:
+                file_token_list.append(self.linguistic(self.tokenization(f.read(), file)))
+        all_token_list = list(chain.from_iterable(file_token_list))  # containing tokens from all documents
+        self.sorting(all_token_list)  # in-place sorting, save memory
+        for token in all_token_list:
+            self.add(token)
+        print('Done in {:.2f}s.'.format(time.time() - start_time))
+        with open(cache_file, 'wb') as f:
+            pickle.dump(self.posting, f, pickle.HIGHEST_PROTOCOL)
+
+    def query(self, query: str):
+        """
+        Perform the same tokenization and lexical transformation on query.
+        Args:
+            query (str): input query
+        Returns:
+            tokens (list): tokens ready for searching the inverted index dictionary.
+        """
+        tokens = query.split()
+        tokens = [self.stemmer.stem(token.translate(self.translator).lower()) for token in tokens]
+        posting_lists = [self.posting[i] if i in self.posting else [] for i in tokens]
+        return self.merge(posting_lists)
+
+    def merge(self, posting_lists: list):
+        """
+        Merge postings lists.
+        Args:
+            posting_lists (list): a list that contains postings lists to be merged.
+        Returns:
+            posting_merged (list): merged postings list.
+        """
+        posting_lists.sort(key=lambda x: len(x))  # in order to start merging from the shortest posting list
+        posting_set = set(posting_lists[0])
+        for i in range(1, len(posting_lists)):
+            posting_set &= set(posting_lists[i])
+        posting_merged = list(posting_set)
+        return posting_merged
+
+    @staticmethod
+    def get_files(dirname: str):
+        """
+        Args:
+            dirname (str): path to directory
+        Returns:
+            file_list (list): full paths to files in the directory
+        """
+        file_list = list()
+        for filename in os.listdir(dirname):
+            file_list.append(os.path.join(dirname, filename))
+        return file_list
+
+    @staticmethod
+    def tokenization(content: str, doc_id: str):
+        """
+        Args:
+            content (str): Document text
+            doc_id(str): Document Id
+        Returns:
+            tokens (list): Token-docId pairs
+        """
+        tokens = list()
+        lines = content.splitlines()
+        for line in lines:
+            token = line.split()  # default split by whitespace
+            tokens.extend(zip(token, len(token) * [doc_id]))
+        return tokens
+
+    def linguistic(self, tokens: list):
+        """
+        Remove punctuation symbols, lowercasing and stemming.
+        Args:
+            tokens (list): Original token-docId pairs
+        Returns:
+            tokens_modified (list): Modified token-docId pairs
+        """
+        tokens_modified = list()
+
+        for pairs in tokens:
+            token = pairs[0].translate(self.translator).lower()  # remove punctuation symbols, lowercasing
+            if token:  # filter out those empty token
+                tokens_modified.append((self.stemmer.stem(token), pairs[1]))  # stemming
+        return tokens_modified
+
+    @staticmethod
+    def sorting(tokens: list):
+        """
+        Perform sorting of the token list, first by tokens (alphabetical order),
+        and then by document ids (alphabetical order).
+        Args:
+            tokens (list): token list to be sorted
+        Returns:
+            tokens_sorted (list): sorted token list
+        """
+        tokens.sort(key=lambda x: (x[0], x[1]))
+
+    def add(self, token_docid):
+        """
+        Transform and add token-docId pairs into postings.
+        Args:
+            token_docid (tuple): token-docId pairs to be added
+        """
+        token = token_docid[0]
+        doc_id = token_docid[1]
+        if token in self.posting:
+            if doc_id not in self.posting[token]:  # collapse identical tokens together
+                self.posting[token].append(doc_id)
         else:
-            self.posting[token[0]] = [1, [token[1]]]
+            self.posting[token] = [doc_id]
 
 
-def merge(posting_lists: list):
-    """
-    Merge postings lists.
-    Args:
-        posting_lists (list): a list that contains postings lists to be merged.
-    Returns:
-        posting_merged (list): merged postings list.
-    """
-    posting_lists.sort(key=lambda x: len(x))  # in order to start merging from the shortest posting list
-    posting_set = set(posting_lists[0])
-    for i in range(1, len(posting_lists)):
-        posting_set &= set(posting_lists[i])
-    posting_merged = list(posting_set)
-    return posting_merged
+
+
+
